ef71ffafcb70f81e8e28ea30d803ead073d30cc0
Seokmin <newacon@naver.com>
2023-01-07 17:31:55 +0200

    drag props in motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   display: flex;
@@ -9,24 +10,50 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
+const BiggerBox = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
   display: flex;
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Box = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 0.7, rotate: 360, transition: { type: "spring", delay: 0.5 } },
+const boxVariants = {
+  hover: { scale: 1.2, rotate: -90 },
+  click: { scale: 0.8, borderRadius: "100%" },
+  drag: {
+    backgroundColor: "rgba(253, 203, 110,1.0)",
+    transition: { duration: 0.5 },
+  },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.05}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
