96043edcf72cd293d47ccbeccaae88e4582da7e4
Seokmin <newacon@naver.com>
2023-01-10 23:29:59 +0200

    slider testing

--- src/App.tsx
+++ src/App.tsx
@@ -9,50 +9,78 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background-color: rgb(255, 215, 70);
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
+  width: 100px;
+  height: 50px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  display: flex;
   position: absolute;
   top: 100px;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: {
+const PrevButton = styled.button`
+  margin: 0 10px;
+`;
+
+const box = {
+  entry: (isBack: boolean) => ({
+    x: isBack ? -200 : 200,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 0.3,
+    },
   },
-  leaving: {
+  exit: (isBack: boolean) => ({
+    x: isBack ? 200 : -200,
     opacity: 0,
     scale: 0,
-  },
+    transition: { duration: 0.3 },
+  }),
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+      <AnimatePresence mode="wait" custom={back}>
+        <Box
+          custom={back}
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
+      <div style={{ display: "flex" }}>
+        <PrevButton onClick={prevPlease}>prev</PrevButton>
+        <button onClick={nextPlease}>next</button>
+      </div>
     </Wrapper>
   );
 }
