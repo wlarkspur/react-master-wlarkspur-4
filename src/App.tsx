8020286ccf29ee8cb05dff4841d101c10d93404b
Seokmin <newacon@naver.com>
2023-01-11 23:40:16 +0200

    NEW: Header, Router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "tv",
+        element: <Tv />,
+      },
+      {
+        path: "search",
+        element: <Search />,
+      },
+    ],
+  },
+]);
