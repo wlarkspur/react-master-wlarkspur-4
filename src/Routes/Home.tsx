6a68cd0b603e6ea8c4c82736562919deeff8cf85
Seokmin <newacon@naver.com>
2023-01-15 14:31:11 +0200

    Slider img: TEXT Added

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -41,11 +41,11 @@ const Overview = styled.p`
 
 const Slider = styled.div`
   position: relative;
-  top: -100px;
+  top: -200px;
 `;
 const Row = styled(motion.div)`
   display: grid;
-  gap: 15px;
+  gap: 10px;
   grid-template-columns: repeat(6, 1fr);
   position: absolute;
   width: 100%;
@@ -58,8 +58,28 @@ const Box = styled(motion.div)<{ bgphoto: string }>`
   height: 200px;
   color: red;
   font-size: 28px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
 const width = useWindowDimensions;
+
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 15px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
     x: window.outerWidth,
@@ -71,6 +91,24 @@ const rowVariants = {
     x: -window.outerWidth,
   },
 };
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: { delay: 0.25, duration: 0.3, type: "tween" },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: { delay: 0.25, duration: 0.3, type: "tween" },
+  },
+};
+
 const offset = 6;
 
 function Home() {
@@ -120,8 +158,16 @@ function Home() {
                   .map((movie) => (
                     <Box
                       key={movie.id}
-                      bgphoto={makeImagePath(movie.backdrop_path, "w500")}
-                    />
+                      whileHover="hover"
+                      initial="normal"
+                      variants={boxVariants}
+                      transition={{ type: "tween" }}
+                      bgphoto={makeImagePath(movie.poster_path, "w500")}
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
