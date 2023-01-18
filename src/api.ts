6fff89c9c777aad0469e7e6baafc4e380f288f82
Seokmin <newacon@naver.com>
2023-01-18 20:19:19 +0200

    ADD: .env complete; API_KEY

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
-const API_KEY = "30a18278aa32c27de99875e2b7b41efe";
+/* const API_KEY = "30a18278aa32c27de99875e2b7b41efe"; */
 const BASE_PATH = "https://api.themoviedb.org/3";
-
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
 interface IMovie {
   id: number;
   backdrop_path: string;
